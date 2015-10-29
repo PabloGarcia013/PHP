f471f25b24ce5f7c62908216ea59da2327239b90
Pablo <pablogarcia.sg@gmail.com>
2015-10-29 16:14:34 +0100

    updated

--- CodigoPersonal/Youtube-lightbox.php
+++ CodigoPersonal/Youtube-lightbox.php
@@ -1,3 +1,14 @@
+<?php
+
+/* 	
+
+Este código muestra en la pantalla actual un pop_up con un video de youtube, haciendo un efecto de lightbox.
+	1. Para ello necesitamos crear un div (id="youtube_pop_up") el cual en un principio esta oculto.
+	2. Cuando el usuario pulse sobre el boton de youtube, mediante javascript, este div pasa de estar oculto a estar visible y añade un iframe que contiene el video de youtube.
+	3. Mediante la funcion "getHbeMusicFileYoutubeEmbedUrl($url)" obtenemos la url valida que se le pasa a la función de javascript para que la añada al iframe.
+*/
+
+?>
 <head>
 <script>
 function show_youtube_ligthbox(id,url_youtube,artist_string,album_string){
@@ -27,6 +38,13 @@ function show_youtube_ligthbox(id,url_youtube,artist_string,album_string){
 </head>
 <body>
 <?php
+
+# Get the correct format of youtube url.
+function getHbeMusicFileYoutubeEmbedUrl($url){
+	preg_match('![?&]{1}v=([^&]+)!', $url . '&', $m);
+	return "https://www.youtube.com/embed/".$m[1]."?autoplay=1&wmode=transparent";
+}
+
 	echo "<div id=\"youtube_pop_up\" class=\"youtube_pop_up\">";
 		echo "<div id=\"youtube_pop_up_wrapper\">";
 			echo "<div id=\"youtube_container\">";
@@ -43,7 +61,9 @@ function show_youtube_ligthbox(id,url_youtube,artist_string,album_string){
 
 	...
 
-	echo "<a href=\"javascript:void(0)\" onclick=\"javascript:show_youtube_ligthbox('youtube_pop_up','youtube_embed_url','$artist_name_string','$album_name_string')\" ><img src=\"youtube.png\"></a><br>";
+	$url_youtube = "video_de_youtube";
+	$url_youtube_final = getHbeMusicFileYoutubeEmbedUrl($url_youtube);
+	echo "<a href=\"javascript:void(0)\" onclick=\"javascript:show_youtube_ligthbox('youtube_pop_up','$url_youtube_final','$artist_name_string','$album_name_string')\" ><img src=\"youtube.png\"></a><br>";
 
 	...
 
